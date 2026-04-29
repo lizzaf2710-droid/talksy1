e6a948c99445d9baad0cb0c367fe0fb012e21a08
Lizzaf2710 <Lizzaf2710@yandex.ru>
2026-04-29 23:57:39 +0300

    fix root route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Talksy backend is running 🚀");
+});
+
 const API_KEY = process.env.API_KEY
 app.post("/chat", async (req, res) => {
   try {
