9ba630542d7feb117e446d5de9f01fe63b8b25ae
Lizzaf2710 <Lizzaf2710@yandex.ru>
2026-05-17 13:14:41 +0300

    Enable CORS for all origins

--- server.py
+++ server.py
@@ -8,6 +8,17 @@ import os
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 # CORS
 app.add_middleware(
     CORSMiddleware,
