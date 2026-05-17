40bf8493d2bb0444481071bd03440aa31b309481
Lizzaf2710 <Lizzaf2710@yandex.ru>
2026-05-17 20:46:51 +0300

    fix cors and merge

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from fastapi.responses import FileResponse
