205a0b961185bb0e0e94a185e19451c59689ba0e
Lizzaf2710 <Lizzaf2710@yandex.ru>
2026-05-18 19:34:59 +0300

    fix groq model

--- server.py
+++ server.py
@@ -77,7 +77,7 @@ async def chat(data: ChatRequest):
     ]
 
     response = client.chat.completions.create(
-        model="llama3-70b-8192",
+        model="llama-3.1-70b-versatile",
         messages=messages,
         temperature=0.7
     )
